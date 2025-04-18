cb8d1288ab7dc9260108a34e5c5fe672da2b7e19
Ken Bonny <bonny.ken+github@gmail.com>
2025-04-18 18:09:19 +0200

    Add LazyCreationResponse for HTTP-aware lazy URL generation
    Introduce `LazyCreationResponse` and its generic version to enable delayed URL generation with thread safety. The class also sets HTTP response headers and status codes for created resources (201). This addition supports easier handling of HTTP response logic in endpoints.

---
+++ src/MoreThanCode.AFrameExample/Shared/LazyCreationResponse.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using JasperFx.CodeGeneration.Frames;
+using Wolverine.Http;
+using Wolverine.Http.Resources;
+
+namespace MoreThanCode.AFrameExample.Shared;
+
+public record LazyCreationResponse : IHttpAware
+{
+    private readonly Lazy<string> _urlCreation;
+
+    public LazyCreationResponse(Func<string> urlCreation) =>
+        _urlCreation = new Lazy<string>(urlCreation, LazyThreadSafetyMode.PublicationOnly);
+
+    public static void PopulateMetadata(MethodInfo method, EndpointBuilder builder)
+    {
+        builder.RemoveStatusCodeResponse(200);
+
+        var create = new MethodCall(method.DeclaringType!, method).Creates.FirstOrDefault()?.VariableType;
+        var metadata = new WolverineProducesResponseTypeMetadata { Type = create, StatusCode = 201 };
+        builder.Metadata.Add(metadata);
+    }
+
+    void IHttpAware.Apply(HttpContext context)
+    {
+        context.Response.Headers.Location = _urlCreation.Value;
+        context.Response.StatusCode = 201;
+    }
+
+    public static LazyCreationResponse<T> For<T>(Func<string> urlCreation, T value) => new(urlCreation, value);
+}
+
+public record LazyCreationResponse<T> : LazyCreationResponse
+{
+    public LazyCreationResponse(Func<string> urlCreation, T value) : base(urlCreation) => Value = value;
+
+    public T Value { get; }
+}
