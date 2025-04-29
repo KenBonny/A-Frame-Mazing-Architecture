903b3f6788f08a2cca19c78e472017d449a99d88
Ken Bonny <bonny.ken+github@gmail.com>
2025-04-29 12:18:45 +0200

    Add integration testing for MetFriends API endpoint
    This commit introduces integration tests for the `/friends/1` endpoint, verifying it returns a 404 Not Found response. It includes a `WebAppFactory` for configuring the test environment and adds the `Microsoft.AspNetCore.Mvc.Testing` package to facilitate testing.

---
+++ src/MoreThanCode.AFrameTests/MetFriendsIntegrationTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Oakton;
+using TUnit.Core.Interfaces;
+using Wolverine;
+
+namespace MoreThanCode.AFrameTests;
+
+public class MetFriendsIntegrationTests
+{
+    [ClassDataSource<WebAppFactory>(Shared = SharedType.PerTestSession)]
+    public required WebAppFactory WebAppFactory { get; init; }
+
+    [Test]
+    public async Task Get_response_bad_request()
+    {
+        var client = WebAppFactory.CreateClient();
+
+        using var response = await client.GetAsync("/friends/1");
+
+        await Assert.That(response.StatusCode).IsEqualTo(HttpStatusCode.NotFound);
+    }
+}
+
+public class WebAppFactory : WebApplicationFactory<Program>, IAsyncInitializer
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        // let Oakton accept --environment variables
+        OaktonEnvironment.AutoStartHost = true;
+
+        // disable all external setup so the integration tests don't start sending out messages
+        builder.ConfigureTestServices(services => services.DisableAllExternalWolverineTransports());
+    }
+
+    public Task InitializeAsync()
+    {
+        // Grab a reference to the server
+        // This forces it to initialise.
+        // By doing it within this method, it's thread safe.
+        // And avoids multiple initialisations from different tests if parallelisation is switched on
+        _ = Server;
+        return Task.CompletedTask;
+    }
+}
